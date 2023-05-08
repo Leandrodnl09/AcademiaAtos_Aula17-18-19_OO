a0c39fd124bd097f81c051189d6d0edae08318e8
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-05-08 19:04:26 -0300

    ConteudoAulaOO

---
+++ AcademiaAtos_OO/Motor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaAtos_OO
+{
+    class Motor
+    {
+        private int cilindradas;
+        private bool ligado;
+        public int Cilindradas { get { return cilindradas; } set { cilindradas = value; } }
+        public bool Ligado { get { return ligado; } set { ligado = value; } }
+
+        public void LigarMotor() 
+        { ligado = true; } 
+        public void DesligarMotor() 
+        { ligado = false; }
+    }
+}
---
+++ AcademiaAtos_OO/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaAtos_OO
+{
+    internal class Pessoa
+    {
+        private string Nome { get; set; }
+        private int Idade { get; set; }
+        private double Salario { get; set; }
+        
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+            Console.WriteLine("Nome: " + nome + " Idade: " + idade);
+        }
+        public Pessoa(int idade)
+        {
+            Idade = idade;
+            Console.WriteLine("Idade: " + idade);
+        }
+
+        private string GetNome()
+        {
+            return Nome;
+        }
+    }
+}
